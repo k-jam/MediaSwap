bdf1f848a52cd12c6640c507d29875801e2b42b1
Chris Albrecht <vegashat@gmail.com>
2011-11-04 21:42:36 -0500

    add user first/last name

--- src/MediaSwap.Core/Models/User.cs
+++ src/MediaSwap.Core/Models/User.cs
@@ -10,6 +10,8 @@ namespace MediaSwap.Core.Models
         public int UserId { get; set; }
         public string Token { get; set; }
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Gravatar { get; set; }
         public string GeoLocation { get; set; }
