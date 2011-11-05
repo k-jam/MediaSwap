02a3e9f8088c1b4075095fd7a6d3ba6077300930
Bradley Lower <bratleylower@gmail.com>
2011-11-04 23:11:46 -0500

    made some changes

--- src/MediaSwap.Core/Models/User.cs
+++ src/MediaSwap.Core/Models/User.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.ComponentModel.DataAnnotations;
 namespace MediaSwap.Core.Models
 {
     public class User
     { 
         public int UserId { get; set; }
+
         public string Token { get; set; }
+        
+        [Required]
+        [Display(Name="User Name")]
         public string UserName { get; set; }
+        
+        [Required]
+        [Display(Name="First Name")]
         public string FirstName { get; set; }
+        
+        [Required]
+        [Display(Name="Last Name")]
         public string LastName { get; set; }
+ 
+        [Required]
+        
         public string Email { get; set; }
+        
         public string Gravatar { get; set; }
+        
+        [Required]
+        [Display(Name="Geo Loocation")]
         public string GeoLocation { get; set; }
+        
         public IEnumerable<Item> Items { get; set; }
 
     }
