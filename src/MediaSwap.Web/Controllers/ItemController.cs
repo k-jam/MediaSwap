5352fca0f9c9029df5481413abf1ea18192fa978
Michael Green <michaelandcara@gmail.com>
2012-02-13 01:18:11 -0600

    added deleteitem methods

--- src/MediaSwap.Web/Controllers/ItemController.cs
+++ src/MediaSwap.Web/Controllers/ItemController.cs
@@ -60,6 +60,32 @@ namespace MediaSwap.Web.Controllers
             addItemViewModel.Status = "Item has been successfully added.";
             return View("Add",addItemViewModel);
         }
+
+        [HttpGet]
+        public ActionResult DeleteItem(int id)
+        {
+            IItemService _itemService = new ItemService();
+            var item = _itemService.GetItem(id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteItem(Item item)
+        {
+            IItemService _itemService = new ItemService();
+
+            //even though the user doesnot have a delete button for loaned items, 
+            //they may have typed the item id within the url ".../Item/DeleteItem/25"
+            //check if item is loaned just for sanity
+            if (_itemService.GetItem(item.ItemId).Queues.Any(e => e.QueueStatusValue == 1))
+            {
+                ModelState.AddModelError("", "Cannot delete a loaned item");
+                return View(item);
+            }
+
+            _itemService.DeleteItem(item.ItemId);
+            return RedirectToAction("Index");
+        }
       
         [HttpGet]
         public ActionResult AddMovie()
