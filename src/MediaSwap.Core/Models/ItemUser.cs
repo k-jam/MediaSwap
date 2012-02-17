6eb1587ea45da3ad83c222c4cb2e680ff60c9f9e
Michael Green <michaelandcara@gmail.com>
2012-02-16 22:51:26 -0600

    removed

---
+++ src/MediaSwap.Core/Models/ItemUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace MediaSwap.Core.Models
+{
+    public class ItemUser
+    {
+        [Key]
+        [Column("Item_ItemId", Order = 0)]
+        public int ItemId { get; set; }
+
+        [Key]
+        [Column("User_UserId", Order = 1)]
+        public int UserId { get; set; }
+
+        public bool Deleted { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ src/MediaSwap.Core/Repositories/MediaSwapTestContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using MediaSwap.Core.Models;
+
+namespace MediaSwap.Core.Repositories
+{
+    public class MediaSwapTestContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+        public DbSet<Item> Item { get; set; }
+        public DbSet<ItemUser> ItemUser { get; set; }
+        public DbSet<ItemType> ItemType { get; set; }
+        public DbSet<Format> Format { get; set; }
+        public DbSet<Queue> Queue { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Item>().HasMany(z => z.Users).WithMany();
+            
+            //base.OnModelCreating(modelBuilder);
+            //Database.SetInitializer<MediaSwapTestContext>(null);
+        }
+    }
+}
